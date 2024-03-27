d9c297f5da152ba34244a7d4322e804d375edbc0
Chelle Wood <chelleyoungw@gmail.com>
2024-03-26 21:04:30 -0500

    added const path to add user input for readme into genearatemarkdown.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const path = require('path');
 
 // TODO: Create an array of questions for user input
 const questions = [];
