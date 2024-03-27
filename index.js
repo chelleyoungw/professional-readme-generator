861ae907568d1c89d1f9ba19377167891ae6b173
Chelle Wood <chelleyoungw@gmail.com>
2024-03-26 22:04:36 -0500

    corrected generateMarkdown const to correct file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const path = require('path');
-const generateMarkdown = require('./utilities/generateMarkdown');
+const generateMarkdown = require('../utilities/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [];
