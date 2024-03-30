6170c33e1894bbd458b6348a6ce41650a1b8bf7e
Chelle Wood <chelleyoungw@gmail.com>
2024-03-30 16:11:32 -0500

    started function for generating markdown for readme in generatemarkdown. added title, description, table of contents, installation, usage and license

--- utilities/generateMarkdown.js
+++ utilities/generateMarkdown.js
@@ -15,7 +15,7 @@ function renderLicenseLink(license) {
 }
   return '';
 }
-// TODO: Create a function that returns the license section of README
+// Creates a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license!== 'none') {
@@ -24,9 +24,37 @@ function renderLicenseSection(license) {
   return '';
 }
 
-// TODO: Create a function to generate markdown for README
+// Creates a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${renderLicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
 
 `;
 }
